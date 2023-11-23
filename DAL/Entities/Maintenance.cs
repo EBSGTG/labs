2c5dbcb9d220afe996a222103339df65bd58fa53
EBSGTG <gerty438@gmail.com>
2023-11-23 18:32:55 +0200

    Added Contracts of Repositories

--- DAL/Entities/Maintenance.cs
+++ DAL/Entities/Maintenance.cs
@@ -2,10 +2,17 @@
 
 public class Maintenance
 {
-    public string MaintenanceID { get; set; }
+    public int MaintenanceID { get; set; }
     public DateTime PlannedTimestamp { get; set; }
     public string Description { get; set; }
 
+    public Maintenance(int maintenanceID, DateTime plannedTimestamp, string description)
+    {
+        MaintenanceID = maintenanceID;
+        PlannedTimestamp = plannedTimestamp;
+        Description = description;
+    }
+    
     public void PlanMaintenance()
     {
         
